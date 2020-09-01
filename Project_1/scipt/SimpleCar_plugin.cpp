dba49d4dee8a99f5d64d598cf30fea8527a8443f
Kishan Patel <imkishan96@gmail.com>
2020-09-01 12:52:57 -0500

    first commit

---
+++ Project_1/scipt/SimpleCar_plugin.cpp
@@ -0,0 +1,29 @@
+#include <gazebo-9/gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+
+namespace gazebo
+{
+    class SimpleCar_plugin : public ModelPlugin
+    {
+    public:
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr )
+        {
+            this->model = _parent;
+            this->updateConnection = event::Events::ConnectWorldUpdateBegin
+            (std::bind(SimpleCar_plugin::OnUpdate, this));
+        }
+
+        void OnUpdate()
+        {   
+            ignition::math::Vector3d init_vel(0.3, 0, 0);
+            this->model->SetLinearVel(init_vel);   
+        }
+    
+    private:
+        physics::ModelPtr model;
+        event::ConnectionPtr updateConnection;
+    };
+    GZ_REGISTER_MODEL_PLUGIN(SimpleCar_plugin)
+}
